fa9e969416f808b46f6dab86989820d900d16330
biswajs <bokakhatmk@hotmail.com>
2021-05-25 10:06:58 +0530

    added package

--- index.js
+++ index.js
@@ -5,3 +5,19 @@
  */
 
 //  Lets start the journey
+class Dembed {
+    constructor(botPrefix, eventType){
+        this.botprefix = botPrefix;
+        this.event = eventType
+    }
+    reply(replyMessage){
+        try{
+            if(replyMessage !== "" || replyMessage !== null)
+            return this.event.reply(replyMessage);
+        }catch(err){
+            return err;
+        }
+    }
+}
+
+module.exports = { Dembed };
